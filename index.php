3d83e44c60cb058400d892e7853091560b73dae6
yusuke000427 <yusuke.sasaki.sys19@morijyobi.ac.jp>
2020-06-16 12:10:58 +0900

    add index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+$dsn = 'mysql:dbname=sample_db;host=localhost;';
+$user = 'yusuke';
+$password = 'Yusuke0427!';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    print "Success\n";
+    $sql = "select * from user;";
+    $resurt =$dbh->query($sql);
+    
+} catch (PDOException $e) {
+    print "Failed: " . $e->getMessage() . "\n";
+    exit();
+}
+   
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+        <meta charset ="utf-8">
+</head>
+<body>
+    <?php foreach( $result as $value ) { ?>
+    <h1><?php echo "$value[name]"; ?></h1>
+    
+    <?php } ?>
+    </body>
+</html>
